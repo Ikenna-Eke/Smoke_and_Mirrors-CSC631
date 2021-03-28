8b5e4b9a68730069711ca58d523863bc43e131fb
mathai1 <mthai1@mail.sfsu.edu>
2021-03-28 12:04:49 -0700

    Created All Scene Files Needed For Game (Not implemented)

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenu : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    //Play Game from Main Menu
+    public void OnPlayClick()
+    {
+        Debug.Log("Play Game");
+    }
+
+    //Exit Game from Main Menu
+    public void OnQuitClick()
+    {
+#if UNITY_EDITOR
+		UnityEditor.EditorApplication.isPlaying = false;
+#else
+		Application.Quit();
+#endif
+    }
+}
