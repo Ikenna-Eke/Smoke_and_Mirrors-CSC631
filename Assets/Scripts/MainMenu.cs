71ed0d55558d1df44d4b1138209924901830fcbf
mathai1 <mthai1@mail.sfsu.edu>
2021-03-31 12:08:05 -0700

    Created Login/Register UI based on Initial designs

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -1,20 +1,50 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class MainMenu : MonoBehaviour
 {
+    private GameObject LoginMenuPanel;
+	private GameObject RegisterMenuPanel;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        LoginMenuPanel = GameObject.Find("Login Menu");
+		RegisterMenuPanel = GameObject.Find("Register Menu");
+
+        LoginMenuPanel.SetActive(true);
+        RegisterMenuPanel.SetActive(false);
     }
 
-    //Play Game from Main Menu
-    public void OnPlayClick()
+    #region LoginMenu
+    //upon clicking filling data and login button click
+    public void OnLoginClick()
     {
-        Debug.Log("Play Game");
+
     }
+    //upon clicking register button on login menu
+    public void OnLoginRegisterClick()
+    {
+        LoginMenuPanel.SetActive(false);
+        RegisterMenuPanel.SetActive(true);
+    }
+    #endregion
+
+    #region RegisterMEnu
+    //after filling out data and sending to database
+    public void OnRegisterClick()
+    {
+        LoginMenuPanel.SetActive(true);
+        RegisterMenuPanel.SetActive(false);
+    }
+    //clicking cancel button
+    public void OnCancelClick()
+    {
+        LoginMenuPanel.SetActive(true);
+        RegisterMenuPanel.SetActive(false);
+    }
+    #endregion
 
     //Exit Game from Main Menu
     public void OnQuitClick()
