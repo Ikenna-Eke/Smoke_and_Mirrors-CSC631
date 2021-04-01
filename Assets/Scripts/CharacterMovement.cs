5575c62af4661720c1e37ab25d65580a2730850e
wleung4 <50734742+wleung4@users.noreply.github.com>
2021-03-31 22:54:51 -0700

    basic character animation fluidity

---
+++ Assets/Scripts/CharacterMovement.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterMovement : MonoBehaviour
+{
+    CharacterController controller;
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    void Update()
+    {
+        if(Input.GetKey("w"))
+        {
+
+        }
+
+    }
+}
